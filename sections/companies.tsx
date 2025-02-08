bad7ecdb08f19cfa5866de4c70cd8cba653d0a34
Oussama Elhousni <oussamaelhousnioe@gmail.com>
2025-02-08 18:36:20 +0100

    animate companies section

--- sections/companies.tsx
+++ sections/companies.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import quantum from "@/assets/images/quantum.svg";
@@ -5,17 +6,29 @@ import twice from "@/assets/images/twice.svg";
 import echoValley from "@/assets/images/echo-valley.svg";
 import acmeCorp from "@/assets/images/acme-corp.svg";
 import apex from "@/assets/images/apex.svg";
+import { motion } from "motion/react";
 
 function Companies() {
   return (
-    <section className="py-[2rem] md:py-[5rem] md:mt-24">
+    <section className="pt-[8rem] pb-[4rem] mt-24">
       <div className="container relative text-center max-w-[1000px] mx-auto overflow-hidden">
-        <p className="description">Already chosen by these market leaders</p>
+        <p className="description text-center">
+          Already chosen by these market leaders
+        </p>
+        <div className="h-full w-1/4 shrink-0 absolute left-0 bg-gradient-to-r from-zinc-950 to-transparent z-10"></div>
+        <div className="h-full w-1/4 shrink-0 absolute right-0 bg-gradient-to-r to-zinc-950 from-transparent z-10"></div>
 
-        <div className="relative">
-          <div className="h-full w-1/4 shrink-0 absolute left-0 bg-gradient-to-r from-zinc-950 to-transparent z-10"></div>
-          <div className="h-full w-1/4 shrink-0 absolute right-0 bg-gradient-to-r to-zinc-950 from-transparent z-10"></div>
-          <div className="flex items-center gap-8  py-8 relative">
+        <div className="w-full flex flex-none">
+          <motion.div
+            className="flex flex-none items-center gap-8 py-8 pr-8"
+            animate={{ x: "-50%" }}
+            transition={{
+              repeat: Infinity,
+              ease: "linear",
+              duration: 20,
+              bounce: 0,
+            }}
+          >
             <Image src={quantum} className="h-8 sm:h-10 w-auto" alt="" />
             <Image src={twice} className="h-8 sm:h-10 w-auto" alt="" />
             <Image src={echoValley} className="h-8 sm:h-10 w-auto" alt="" />
@@ -26,7 +39,18 @@ function Companies() {
             <Image src={echoValley} className="h-8 sm:h-10 w-auto" alt="" />
             <Image src={acmeCorp} className="h-8 sm:h-10 w-auto" alt="" />
             <Image src={apex} className="h-8 sm:h-10 w-auto" alt="" />
-          </div>
+
+            <Image src={quantum} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={twice} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={echoValley} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={acmeCorp} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={apex} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={quantum} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={twice} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={echoValley} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={acmeCorp} className="h-8 sm:h-10 w-auto" alt="" />
+            <Image src={apex} className="h-8 sm:h-10 w-auto" alt="" />
+          </motion.div>
         </div>
       </div>
     </section>
